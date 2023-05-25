2a15b71e8f859657f632d6622c2ca83ce3ca1ddd
liamcrowe02 <liamcrowe2502@gmail.com>
2023-05-25 15:37:58 +0100

    Added the Hashmap which includes an addContat and a list all Contacts functions. The do not work yet.

---
+++ src/phoneBook.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class phoneBook {
+
+    Map<String, addContact> phoneBook = new HashMap<>();
+
+    public void addContact(String name, String phoneNum) {
+        addContact contacts = new addContact(name, phoneNum);
+        phoneBook.put(name, contacts);
+    }
+
+    public void listContacts() {
+        for (Map.Entry<String,addContact> entry : phoneBook.entrySet()){
+            addContact contacts = entry.getValue();
+            System.out.println("Name: " + contacts.getName() + " " +
+                               "Phone Number: " + contacts.getPhoneNum() + '\'');
+        }
+    }
+}
