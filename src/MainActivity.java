4a23e124b070be378c46a3db97d501163bb044e3
liamcrowe02 <liamcrowe2502@gmail.com>
2023-05-27 17:56:34 +0100

    Scanner had now been implemented, but you can only add one contact in the phonebook.

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class MainActivity {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
         phoneBook phonebook = new phoneBook();
-        phonebook.addContact("John", "1234567890");
-        phonebook.addContact("Alice", "9876543210");
-        phonebook.addContact("Bob", "4567890123");
+
+        System.out.println("Enter username");
+        String userName = myObj.nextLine();
+
+        System.out.println("Enter phone number");
+        String phoneNo = myObj.nextLine();
+
+
+        phonebook.addContact(userName, phoneNo);
 
         phonebook.listContacts();
     }
